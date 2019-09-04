6cc316833802a207e5b434f3d998cb91cc7dfed4
asconsulting <andrew@andrewstevens.consulting>
2019-09-04 04:17:43 -0400

    Create default.php

---
+++ system/modules/isotope_product_attachment/languages/en/default.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Isotope Product Attachment
+ *
+ * Copyright (C) 2019 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/isotope_product_attachment
+ * @link       https://andrewstevens.consulting
+ */
+ 
+
+$GLOBALS['TL_LANG']['ATTR']['productAttachment']        = array('Product Attachment', 'Attach a file to a product.');
